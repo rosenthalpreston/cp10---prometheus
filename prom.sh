856af3239f727d4fd2fa2ada7206a05042182bc5
rosenthalpreston <129431344+rosenthalpreston@users.noreply.github.com>
2024-01-23 10:08:19 +0100

    Create prom.sh

---
+++ prom.sh
@@ -0,0 +1,89 @@
+Description=Prometheus
+Wants=network-online.target
+After=network-online.target
+
+StartLimitIntervalSec=500
+StartLimitBurst=5
+
+[Service]
+User=prometheus
+Group=prometheus
+Type=simple
+Restart=on-failure
+RestartSec=5s
+ExecStart=/usr/local/bin/prometheus \
+  --config.file=/etc/prometheus/prometheus.yml \
+  --storage.tsdb.path=/data \
+  --web.console.templates=/etc/prometheus/consoles \
+  --web.console.libraries=/etc/prometheus/console_libraries \
+  --web.listen-address=0.0.0.0:9090 \
+  --web.enable-lifecycle
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl enable prometheus
+sudo systemctl start prometheus
+
+##Install Node Exporter and Create Service for Node Exporter
+sudo useradd --system --no-create-home --shell /bin/false node_exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz
+tar -xvf node_exporter-1.6.1.linux-amd64.tar.gz
+sudo mv node_exporter-1.6.1.linux-amd64/node_exporter /usr/local/bin/
+rm -rf node_exporter*
+sudo cat > /etc/systemd/system/node_exporter.service << EOF
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+StartLimitIntervalSec=500
+StartLimitBurst=5
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+Restart=on-failure
+RestartSec=5s
+ExecStart=/usr/local/bin/node_exporter --collector.logind
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl enable node_exporter
+sudo systemctl start node_exporter
+
+##Install Grafana
+$ sudo apt-get update
+$ sudo apt-get install -y apt-transport-https software-properties-common
+$ wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+$ echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+$ sudo apt-get update
+$ sudo apt-get -y install grafana
+$ sudo systemctl enable grafana-server
+$ sudo systemctl start grafana-server
+
+
+4--Commands to check services
+$ sudo systemctl status prometheus
+$ sudo systemctl status node_exporter
+$ sudo systemctl status grafana-server
+
+5--Add job for node exporter in prometheus
+$ cd /etc/prometheus/, ls, $ sudo nano prometheus.yml and below the job of prometheus, add job for node exporter
+  - job_name: 'node_exporter'
+    static_configs:
+      - targets: ['localhost:9100']
+
+6--Check the indentatio of the prometheus config file with below command
+$ promtool check config /etc/prometheus/prometheus.yml
+
+7--Reload the Prometheus configuration
+$ curl -X POST http://localhost:9090/-/reload
+
+8--On monitoring server go to $ cd /etc/prometheus/ & $ sudo nano prometheus.yml and add job for Jenkins
+- job_name: 'jenkins'
+    metrics_path: '/prometheus'
+    static_configs:
+      - targets: ['localhost:8080']
